15c6fe9edfdd4f159075a2e0ef68efd27f48c85c
Bili345679 <3130759140@qq.com>
2023-02-18 15:34:00 +0800

    第一次上传

---
+++ demo.py
@@ -0,0 +1,47 @@
+from easy_input import *
+
+# mode用来设置颜色
+# 可取值["info","warning","error", "red", "green", "yellow", "blue", "magenta", "cyan", "white", False]
+# 默认为 info
+
+# 输出
+cprint("这是一段字符串")
+cprint("这是一段警告字符串", mode="warning")
+cprint("这是一段危险字符串", mode="error")
+cprint("这是一段不带时间的字符串", show_time=False)
+cprint("这是一段不带时间，调整颜色的字符串", mode="cyan", show_time=False)
+print("---------")
+cprint("!@#!@#!@#", "info", show_time=False)
+cprint("!@#!@#!@#", "warning", show_time=False)
+cprint("!@#!@#!@#", "error", show_time=False)
+print("---------")
+cprint("!@#!@#!@#", "red", show_time=False)
+cprint("!@#!@#!@#", "green", show_time=False)
+cprint("!@#!@#!@#", "yellow", show_time=False)
+cprint("!@#!@#!@#", "blue", show_time=False)
+cprint("!@#!@#!@#", "magenta", show_time=False)
+cprint("!@#!@#!@#", "cyan", show_time=False)
+cprint("!@#!@#!@#", "white", show_time=False)
+cprint("!@#!@#!@#", False, show_time=False)
+
+
+# 输入
+# 格式筛选
+int_val = cinput("输入整数", "int", default=1)
+print(int_val)
+float_val = cinput("输入浮点数", "float", default=2.2)
+print(float_val)
+str_val = cinput("输入字符串", "str", default="string")
+print(str_val)
+
+# 显示内容
+# 不显示时间
+val_1 = cinput("input_1", show_time=False)
+print(val_1)
+# 不显示返回值
+val_2 = cinput("input_2", show_val=False)
+print(val_2)
+
+# 不能为空
+val_3 = cinput("input_3", not_empty_flag=True)
+print(val_3)
---
+++ easy_input.py
@@ -0,0 +1,93 @@
+import time
+from termcolor import colored
+
+# print模式对应颜色
+def mode_to_color(mode="info"):
+    if not mode:
+        return "white"
+
+    mode_to_color_dict = {"info": "green", "warning": "yellow", "error": "red"}
+    color_list = ["red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+    if mode in mode_to_color_dict:
+        return mode_to_color_dict[mode]
+    elif mode in color_list:
+        return mode
+    else:
+        return "white"
+
+
+# 带时间打印
+def cprint(val, mode="info", show_time=True):
+    if show_time:
+        print(add_data_time_head(str(val), mode))
+    else:
+        print(colored(val, mode_to_color(mode)))
+    return val
+
+
+# 带格式检测的输入
+#   type 输入内容的格式
+#   default 默认值
+#   not_empty_flag 禁止为空(如果有默认值时，返回默认值)
+#   mode 输出颜色 ["info","warning","error", "red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+#   show_time 展示时间
+#   show_val 展示输入值
+def cinput(
+    org_key,
+    type="str",
+    default="",
+    not_empty_flag=False,
+    mode="info",
+    show_time=True,
+    show_val=True,
+):
+    try:
+        key = org_key
+        if default != "":
+            key += " (默认值%s)" % (str(default))
+
+        if show_time:
+            key = add_data_time_head(key, mode)
+        else:
+            key = colored(key, mode_to_color(mode))
+
+        str_val = input(key)
+
+        # 无输入
+        if str_val == "":
+            if default != "":
+                str_val = default
+                val = default
+            elif not_empty_flag:
+                cprint("该值不能为空", "error")
+                return cinput(org_key, type, default, not_empty_flag, mode)
+            else:
+                str_val = default
+                val = default
+        elif type == "int":
+            val = int(str_val)
+        elif type == "float":
+            val = float(str_val)
+        else:
+            val = str_val
+    except Exception:
+        return cinput(org_key, type, default, not_empty_flag, mode)
+
+    if show_val:
+        cprint("\t" + str(str_val), mode, show_time)
+    return val
+
+
+# 给字符串添加时间前缀
+def add_data_time_head(str, mode=False, timestamp=False):
+    return (
+        colored("[" + get_data_time(timestamp) + "]", mode_to_color(mode)) + " " + str
+    )
+
+
+# 获取格式化时间
+def get_data_time(timestamp=False):
+    if not timestamp:
+        timestamp = time.time()
+    timestamp = time.localtime(timestamp)
+    return time.strftime("%Y-%m-%d_%H-%M-%S", timestamp)
