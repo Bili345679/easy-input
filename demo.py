1f50b4c44fe50ccce13174d42c65998085e1f8b8
wyh <3130759140@qq.com>
2023-02-22 12:13:55 +0800

    ezprint颜色参数完成

--- demo.py
+++ demo.py
@@ -1,21 +1,49 @@
 from easy_input import *
 from termcolor import cprint
 
-print(color_msg("hello,world", ["underline"]))
-# print(color_msg("hello,world", "red"))
-# print(color_msg("hello,world", "info"))
-# print(color_msg(["hello,world", "hello,python"], ["red", "blue"]))
-# print(color_msg(["hello,world", "hello,python"], ["red", ["blue", ["bold"]]]))
-# print(
-#     color_msg(
-#         ["hello,world", "hello,python"],
-#         ["red", ["blue", ["bold"]]],
-#         [["underline"]],
-#     )
-# )
+print(color_str("hello,world", ["underline"]))
+print(color_str("hello,world", "red"))
+print(color_str("hello,world", "info"))
+print(color_str(["hello,world", "hello,python"], ["red", "blue"]))
+print(color_str(["hello,world", "hello,python"], ["red", ["blue", ["bold"]]]))
+print(
+    color_str(
+        ["hello,world", "hello,python"],
+        ["red", ["blue", ["bold"]]],
+        ["underline"],
+    )
+)
+print(
+    color_str(
+        ["hello,world", "hello,python"],
+        ["red", ["blue", ["bold"]]],
+        [["underline"]],
+    )
+)
+print(
+    color_str(
+        ["hello,world", "hello,python"],
+        ["red", ["blue", ["bold"]]],
+        ["on_green", ["underline"]],
+    )
+)
+print(
+    color_str(
+        ["hello,world", "hello,python"],
+        ["red", ["blue", ["bold"]]],
+        ["on_green"],
+    )
+)
+print(
+    color_str(
+        ["hello,world", "hello,python"],
+        ["red", ["blue", ["bold"]]],
+        "on_green",
+    )
+)
 
-# ezprint("Hello,World!")
-# ezprint("Hello,World!", "warning")
-# ezprint("Hello,World!", "error")
-# ezprint("Hello,World!", "error", show_time=False)
-# ezprint("Hello,World!", default_color=[["underline"]])
+ezprint("Hello,World!")
+ezprint("Hello,World!", "warning")
+ezprint("Hello,World!", "error")
+ezprint("Hello,World!", "error", show_time=False)
+ezprint("Hello,World!", def_color=[["underline"]])
--- easy_input.py
+++ easy_input.py
@@ -1,193 +1,200 @@
-# __all__ = []
-
 import time
-from termcolor import colored
+from termcolor import colored, cprint
 import termcolor
 
-
-def in_tc_c(param):
-    info_type_list = ["info", "warning", "error"]
+# 是否是 termcolor 文字颜色参数
+def is_tc_c(param):
     if not isinstance(param, str):
         return False
+    return param in termcolor.COLORS or param in ["info", "warning", "error"]
 
-    return (param in termcolor.COLORS) or (param in info_type_list)
 
-
-def in_tc_h(param):
+# 是否是 termcolor 背景色颜色参数
+def is_tc_h(param):
     if not isinstance(param, str):
         return False
-
     return param in termcolor.HIGHLIGHTS
 
 
-def in_tc_a(param):
-    print(param)
+# 是否是 termcolor 额外参数
+def is_tc_a(param):
     if not isinstance(param, list):
         return False
-    for each_param in param:
-        if each_param not in termcolor.ATTRIBUTES:
+    for each in param:
+        if not isinstance(each, str):
+            return False
+        if each not in termcolor.ATTRIBUTES:
             return False
     return True
 
 
-# 颜色参数类型
-def in_tc_p(param):
-    if in_tc_c(param):
+# 是否是 termcolor 参数
+def is_tc_p(param):
+    if is_tc_c(param):
         return 0
-    elif in_tc_h(param):
+    elif is_tc_h(param):
         return 1
-    elif in_tc_a(param):
+    elif is_tc_a(param):
         return 2
     return False
 
 
-# 是否只是一条颜色信息
-def is_single_color_info(param):
-    if isinstance(param, str):
+# print模式对应颜色
+def mode_to_color(mode):
+    mode_to_color_dict = {"info": "green", "warning": "yellow", "error": "red"}
+    if mode in mode_to_color_dict:
+        return mode_to_color_dict[mode]
+    else:
+        return mode
+
+
+# 是否是单条颜色信息
+def is_single_color(color):
+    if isinstance(color, str):
+        if is_tc_p(color) is not False:
+            return True
+        else:
+            raise "color 参数非 termcolor 可选参数"
+
+    if not isinstance(color, list):
+        raise "color 参数仅为 str 型或 list 型"
+
+    if is_tc_a(color):
         return True
 
-    if len(param):
-        return False
-
-    param_type_list = [0, 0, 0]
-    for each_param in param_type_list:
-        if in_tc_c(each_param):
-            param_type_list[0] += 1
-        elif in_tc_h(each_param):
-            param_type_list[1] += 1
-        elif in_tc_h(each_param):
-            param_type_list[2] += 1
+    param_count_list = [0, 0, 0]
+    for each_param in color:
+        param_type = is_tc_p(each_param)
+        # 参数不是 termcolor 的参数，可能是 color 列表
+        if param_type is False:
+            return False
         else:
+            param_count_list[param_type] += 1
+        if 2 in param_count_list:
             return False
-        if 2 in param_type_list:
-            return False
-
     return True
 
 
-# 给颜色信息添加颜色参数
-def add_color_param(color, default_color):
-
-    if color[0] == None:
-        color[0] = default_color[0]
-
-    if color[1] == None:
-        color[1] = default_color[1]
-
-    if color[2] == None:
-        color[2] = default_color[2]
-    elif default_color[2] is not None:
-        for each_a in default_color[2]:
-            if each_a not in color[2]:
-                color[2].append(each_a)
-
-    return color
-
-
-# 格式化参数类型
-def format_color_param(color):
+# 格式化颜色参数(参数仅为单条数据)
+def format_color(param):
     param_list = [None, None, None]
-    color_type = in_tc_p(color)
-    if color_type is False:
+    param_type = is_tc_p(param)
+    if param_type is False:
         # 参数列表
-        for each_param in color:
+        for each_param in param:
             # 颜色类型
-            param_type = in_tc_p(each_param)
+            param_type = is_tc_p(each_param)
             # 未知类型
             if param_type is False:
                 continue
             if param_type == 0:
-                each_param = get_color(each_param)
+                each_param = mode_to_color(each_param)
             param_list[param_type] = each_param
     else:
         # 单一参数
-        if color_type == 0:
-            color = get_color(color)
-        param_list[color_type] = color
+        if param_type == 0:
+            param = mode_to_color(param)
+        param_list[param_type] = param
 
     return param_list
 
 
-# print模式对应颜色
-def get_color(color):
-    mode_to_color_dict = {"info": "green", "warning": "yellow", "error": "red"}
-    if color in mode_to_color_dict:
-        return mode_to_color_dict[color]
-    else:
-        return color
+# 添加默认颜色
+def add_def_color(color, def_color):
+    if color[0] is None and def_color[0] is not None:
+        color[0] = def_color[0]
+    if color[1] is None and def_color[1] is not None:
+        color[1] = def_color[1]
+
+    if color[2] is None and def_color[2] is not None:
+        color[2] = def_color[2]
+    elif def_color[2] is not None:
+        for each_def_color_p in def_color[2]:
+            if each_def_color_p not in color[2]:
+                color[2].append(each_def_color_p)
+    return color
 
 
-# 给文字添加颜色
-def color_msg(msg, color, default_color=False):
-    print("#####################")
-    print(msg)
-    print(color)
-    print(default_color)
-    # 格式化颜色并转换成列表
-    if is_single_color_info(color):
-        color = [format_color_param(color)]
+def color_str(string, color, def_color=False):
+    # color
+    if is_single_color(color):
+        color_list = [format_color(color)]
     else:
-        new_color_list = []
+        color_list = []
         for each_color in color:
-            new_color_list.append(format_color_param(each_color))
-        color = new_color_list
+            if not is_single_color(each_color):
+                raise "颜色格式错误"
+            color_list.append(format_color(each_color))
 
-    # 默认颜色
-    if default_color is not False:
-        print("asd")
-        default_color = format_color_param(default_color)
-        print("asd")
-        new_color_list = []
-        for index in range(len(msg)):
-            print(color[index], "140")
-            if index >= len(color):
-                new_color_list.append(default_color)
-            else:
-                new_color_list.append(add_color_param(color[index], default_color))
-        color = new_color_list
-    print(color)
+    # def_color
+    if def_color is not False:
+        if is_single_color(def_color):
+            def_color = format_color(def_color)
+        else:
+            def_color = format_color(def_color[0])
 
-    # 单独一条msg
-    if isinstance(msg, str):
-        return color_single_msg(msg, format_color_param(color[0]))
+        defed_color_list = []
+        for each_color in color_list:
+            defed_color_list.append(add_def_color(each_color, def_color))
+        color_list = defed_color_list
+    # str
+    if isinstance(string, str):
+        return color_single_str(string, color_list[0])
 
-    # 多条msg
-    colored_msg_list = []
+    if not isinstance(string, list):
+        raise "string 参数仅为 str 型或 list 型"
 
-    for index in range(len(msg)):
-        if index >= len(color):
-            colored_msg_list.append(msg[index])
-            continue
+    colored_str_list = []
+    for index in range(len(string)):
+        if not isinstance(string[index], str):
+            raise "string 参数格式错误"
 
-        colored_msg_list.append(color_single_msg(msg[index], color[index]))
+        this_color = (
+            color_list[index]
+            if index < len(color_list)
+            else (def_color if def_color is not False else [None, None, None])
+        )
 
-    return "".join(colored_msg_list)
+        colored_str_list.append(color_single_str(string[index], this_color))
+
+    return "".join(colored_str_list)
 
 
 # 给一段文字添加颜色
-def color_single_msg(msg, color):
-    color_param = (msg,)
+def color_single_str(string, color):
+    color_param = (string,)
+
     color_param += tuple(color)
+
     return colored(*color_param)
 
 
-# 带时间打印
-def ezprint(msg, color="info", show_time=True, default_color=False):
+# ezprint
+#   msg 信息
+#       str
+#       list
+#   color 颜色参数
+#       color
+#           list
+#               lsit[list]
+#   show_time 显示时间
+#   def 默认颜色参数
+#       color
+#           list
+#               lsit[list]
+
+
+def ezprint(msg, color="info", show_time=True, def_color=False, end=None):
+    if isinstance(msg, str):
+        msg = [msg]
+    elif not isinstance(msg, list):
+        raise "ezinput msg 参数 仅为 str 型或 list 型"
+
     if show_time:
-        date_str = "[" + get_date_str() + "]\t"
-        if is_single_color_info(color):
-            msg = color_msg(date_str, color) + msg
-        else:
-            if isinstance(msg, str):
-                msg = [date_str, msg]
-            else:
-                msg = [date_str] + msg
-    print(color_msg(msg, color, default_color))
+        msg = [get_date_str() + "\t"] + msg
 
+    print(color_str(msg, color, def_color))
 
-# 带格式检测的输入
-
-# 给字符串添加时间前缀
 
 # 获取格式化时间
 def get_date_str(timestamp=False):
@@ -195,8 +202,3 @@ def get_date_str(timestamp=False):
         timestamp = time.time()
     timestamp = time.localtime(timestamp)
     return time.strftime("%Y-%m-%d %H:%M:%S", timestamp)
-
-
-# 对象是否为字符串
-def is_string(val):
-    return isinstance(val, str)
